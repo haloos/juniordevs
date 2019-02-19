420e1e7c426e3f46e193d27936f219d7c81e8bfd
Jason Paredes <haloos@me.com>
2019-02-19 18:31:46 -0500

    added User.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 
@@ -12,7 +17,12 @@ const db = require('./config/keys').mongoURI;
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello')); 
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
