dc228a098ed516e69e89bbb47f153e322c7a8184
Jason Paredes <haloos@me.com>
2019-03-14 19:40:33 -0400

    added login password

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 
 // Load Input Validation 
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -66,7 +67,14 @@ router.post('/register', (req, res) => {
 // @route         GET api/users/login
 // @description   Login User / Returning JWT Token
 // @access        Public
-router.post('/login', (req, res) => {
+router.post('/login', (req, res) => { 
+  const { errors, isValid } = validateLoginInput(req.body); 
+
+  // Check Validation
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
  
@@ -74,8 +82,9 @@ router.post('/login', (req, res) => {
   User.findOne({email}) .then(user => {
     
       // Check for user
-      if(!user) {
-        return res.status(404).json({email: 'User not found'});
+      if(!user) { 
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       } 
 
       // Check Password
@@ -97,7 +106,8 @@ router.post('/login', (req, res) => {
              });
           });
         } else {
-          return res.status(400).json({password: 'Password incorrect'});
+          errors.password = 'Password incorrect';
+          return res.status(400).json(errors);
         }
       })
     });
